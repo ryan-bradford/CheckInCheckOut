6ea3a23ecd318e0f58b9add6d8bee1f11f8d3179
ryanb <ryanbradford333@gmail.com>
2016-06-30 11:50:08 -0400

    Initial Commit

---
+++ CheckInCheckOutTests/CheckInCheckOutTests.swift
@@ -0,0 +1,36 @@
+//
+//  CheckInCheckOutTests.swift
+//  CheckInCheckOutTests
+//
+//  Created by Ryan on 6/30/16.
+//  Copyright © 2016 SDMFoundation. All rights reserved.
+//
+
+import XCTest
+@testable import CheckInCheckOut
+
+class CheckInCheckOutTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
